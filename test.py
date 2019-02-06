9641b41bde6cb05cd8ea01d43024fe012b46ec05
frankye8998 <39440830+frankye8998@users.noreply.github.com>
2019-02-05 20:57:40 -0500

    More proper coding

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import time
+import urllib.parse
 
-def bus_coming(stop):
-    stop = str(stop)
+def BusComing(stop : str):
     fime = time.localtime(time.time())
-    time_hour = str(fime.tm_hour)
-    time_minute = str(fime.tm_min)
-
-    url = "https://tripplanner.yrt.ca/hiwire?Date=Today&TimeHour="+time_hour+"&TimeMinute="+time_minute+"&Meridiem=a&.a=iNextBusFind&.s=BXuxJEvaCS6XpszEt2kGsQ&ShowTimes=1&NumStopTimes=5&GetSchedules=1&EndGeo=&StopAbbr="+stop+"&.a=iNextBusFind"
-    html = requests.get(url)
-    soup = bs(html.text,'html.parser')
-
-    table = soup.find_all("table",{"class":"datatable"})[0]
-    our_list = []
+    table = bs(requests.get(f'https://tripplanner.yrt.ca/hiwire?{urllib.parse.urlencode({"Date": "Today", "TimeHour": fime.tm_hour, "TimeMinute": fime.tm_min, "Meridiem": "a", ".a": "iNextBusFind", ".s": "BXuxJEvaCS6XpszEt2kGsQ", "ShowTimes": "1", "NumStopTimes": "5", "GetSchedules": "1", "EndGeo": "", "StopAbbr": str(stop)})}').text, "html.parser").find("table", {"class": "datatable"})
+    BusList = []
     for i in range(1,len(table.contents)):
         bus = table.contents[i]
         route = bus.contents[1].contents[0]
         scheduled_arrival = bus.contents[3].contents[0]
         estimated_arrival = bus.contents[5].contents[0]
-        our_list.append([route, scheduled_arrival,estimated_arrival])
-    return our_list
+        BusList.append([route, scheduled_arrival,estimated_arrival])
+    return BusList
+
+# Testing
+print(BusComing(1194))
